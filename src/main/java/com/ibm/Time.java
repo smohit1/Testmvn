22725173900b90f7cbc9c62d28f3dd670d39e90e
Mohit Sharma <sharma-mohit1@hotmail.com>
2019-03-05 13:59:09 +0530

    first

---
+++ src/main/java/com/ibm/Time.java
@@ -0,0 +1,13 @@
+package com.ibm;
+
+/**
+ * Hello world!
+ *
+ */
+public class Time
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Mohit" );
+    }
+}
---
+++ src/test/java/com/ibm/TimeTest.java
@@ -0,0 +1,22 @@
+package com.ibm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TimeTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+
+    System.out.println("My name is Mohit");
+    }
+}
